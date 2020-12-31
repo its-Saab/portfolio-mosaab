030e286e5afa13b39573f4809a49a89f987b454e
its-Saab <musaababs@gmail.com>
2020-12-31 15:56:08 +0100

    Updated to accept initial value + provide dates

--- components/PortfolioForm.js
+++ components/PortfolioForm.js
@@ -3,16 +3,22 @@ import { useForm } from "react-hook-form";
 import {useState, useEffect} from "react";
 
 
-const PortfolioForm = ({onSubmit}) => {
+const PortfolioForm = ({onSubmit, initialData = {}}) => {
 
     const [startDate, setStartDate] = useState(null)
     const [endDate, setEndDate] = useState(null)
-    const {register, handleSubmit, setValue} = useForm()
+    const {register, handleSubmit, setValue} = useForm({defaultValues: initialData})
 
     useEffect(() => {
       register({name: 'startDate'})
       register({name: 'endDate'})
     }, [register])
+
+    useEffect(() =>{
+        const {startDate, endDate} = initialData
+        if (startDate) {setStartDate(new Date(startDate))}
+        if (endDate) {setEndDate(new Date(endDate))}
+    },[initialData])
 
     const handleDateChange = (dateType,setDate) => date => {
       setValue(dateType, date);
