0fca8e096d4835c8c3a10e12ad497b6187a6f7fb
its-Saab <musaababs@gmail.com>
2021-01-01 17:26:50 +0100

    Updated: delete functionality  to btn/portfolios

--- pages/actions/portfolios.js
+++ pages/actions/portfolios.js
@@ -6,11 +6,11 @@ import { fetcher } from '@/pages/actions';
 
  const createPortfolio = data =>  axios.post('/api/v1/portfolios',data);
  const updatePortfolio = (id, data) =>  axios.patch(`/api/v1/portfolios/${id}`,data);
-
+const deletePortfolio = id => axios.delete(`/api/v1/portfolios/${id}`);
 
 export const useCreatePortfolio = () => useApiHandler(createPortfolio);
 export const useUpdatePortfolio = () => useApiHandler(updatePortfolio);
-
+export const useDeletePortfolio = () => useApiHandler(deletePortfolio);
 
 
 export const useGetPortfolio = (id) => {
--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import BaseLayout from '@/components/layouts/BaseLayout'
 import BasePage from '@/components/BasePage'
 import {useGetUser} from '@/pages/actions/user'
+import {useDeletePortfolio} from '@/pages/actions/portfolios'
 import PortfolioApi from '@/lib/api/portfolios'
 import PortfolioCard from '@/components/shared/PortfolioCard'
 import { Row, Col, Button} from 'reactstrap';
@@ -10,8 +11,17 @@ import {isAuthorized} from '@/utils/auth0'
 
 const Portfolios = ({portfolios}) => {
     const router = useRouter()
+    const [deletePortfolio , {error,data}] = useDeletePortfolio()
 //destructrise the return from the function useGetPosts/useSwr that receives the post from the api
     const {data: dataU, loading:loadingU} = useGetUser()
+
+    const _deletePortfolio = async (e, portfolioID) => {
+        e.stopPropagation()
+        const isConfirm = confirm("Are you sure?")
+        if (isConfirm){
+            await deletePortfolio(portfolioID)
+        }
+    }
             return(
                 <>
             <Head>
@@ -43,7 +53,9 @@ const Portfolios = ({portfolios}) => {
                                     router.push('/portfolios/[id]/edit', `/portfolios/${portfolio._id}/edit`)}}
                                 className="mr-2"
                                 color= "warning">Edit</Button>
-                            <Button color="danger">Delete</Button>
+                            <Button
+                            onClick={(e) => _deletePortfolio(e, portfolio._id)}
+                            color="danger">Delete</Button>
                             </>
                         }
                         </PortfolioCard>
