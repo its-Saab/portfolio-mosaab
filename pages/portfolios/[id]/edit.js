7eb3d7feeb5ec2316daf7e8fed8b2bd053d5478f
its-Saab <musaababs@gmail.com>
2021-01-01 14:04:41 +0100

    Added update-hook to onSubmit

--- pages/portfolios/[id]/edit.js
+++ pages/portfolios/[id]/edit.js
@@ -1,28 +1,35 @@
 import React from 'react'
 import BaseLayout from '@/components/layouts/BaseLayout'
-import {withRouter} from 'next/router'
 import BasePage from '@/components/BasePage'
 import withAuth from '@/hoc/withAuth'
 import {useRouter} from 'next/router'
 import {useGetPortfolio} from '@/pages/actions/portfolios'
 import {Col, Row} from 'reactstrap'
 import PortfolioForm from '@/components/PortfolioForm'
+import { useUpdatePortfolio } from '@/pages/actions/portfolios'
 
 const PortfolioEdit = ({user}) => {
   const router = useRouter()
-  const {data} = useGetPortfolio(router.query.id)
+  const [updatePortfolio, {data,error,loading}] = useUpdatePortfolio()
+  const {data: initialData} = useGetPortfolio(router.query.id)
+
+  //this function is to add the id from router.query.id to updatePortfolio
+  const _updatePortfolio = data => {
+     updatePortfolio(router.query.id, data)
+  }
   return(
     <BaseLayout user={user} loading={false}>
     <BasePage header="Portfolio Edit:">
-    {data &&
       <Row>
         <Col md="8">
-          <PortfolioForm
-          onSubmit={(data => alert(JSON.stringify(data)))}
-          initialData={data}/>
+          {initialData &&
+            <PortfolioForm
+              onSubmit={_updatePortfolio}
+              initialData={initialData}
+            />
+          }
         </Col>
       </Row>
-    }
     </BasePage>
     </BaseLayout>
   )
