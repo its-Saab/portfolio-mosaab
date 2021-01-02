0c8dbf34525bd3a73426235d1d770c7b37e3a2b7
its-Saab <musaababs@gmail.com>
2021-01-02 21:12:12 +0100

    UPDT: moment package-content fix

--- pages/portfolios/[id]/index.js
+++ pages/portfolios/[id]/index.js
@@ -3,20 +3,35 @@ import BaseLayout from 'components/layouts/BaseLayout'
 import BasePage from 'components/BasePage'
 import {useGetUser} from 'actions/user'
 import PortfolioApi from 'lib/api/portfolios'
+import {formatDate} from 'helpers/function'
 
 const Portfolio = ({portfolio}) => {
   const {data: dataU,loading:loadingU} = useGetUser()
   return(
     <BaseLayout
+    navClass="transparent"
     user={dataU}
     loading={loadingU}
     >
     <BasePage
+    noWrapper
+    indexPage
+    metaDescription={portfolio.description}
     title={`${portfolio.title}`}
-    header="Portfolio Detail:">
-      {
-        JSON.stringify(portfolio)
-      }
+    >
+      <div className="portfolio-detail">
+      <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+        <main role="main" class="inner page-cover">
+          <h1 class="cover-heading">{portfolio.title}</h1>
+          <p class="lead dates">{formatDate(portfolio.startDate)}-{formatDate(portfolio.endDate) || 'Present'}</p>
+          <p class="lead info mb-0">{portfolio.jobTitle} | {portfolio.company} | {portfolio.location}</p>
+          <p class="lead">{portfolio.description}</p>
+          <p class="lead">
+            <a href={portfolio.companyWebsite} target="_" class="btn btn-lg btn-secondary">Visit Company's website</a>
+          </p>
+        </main>
+      </div>
+    </div>
     </BasePage>
     </BaseLayout>
   )
