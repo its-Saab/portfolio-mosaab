c7813f84193035b1574de54342882b442aa1278c
its-Saab <musaababs@gmail.com>
2021-01-02 15:47:14 +0100

    UPDT: added activeLink to header elements

---
+++ components/shared/ActiveLink.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import {useRouter} from 'next/router'
+import React, { Children } from 'react'
+
+
+const ActiveLink = ({children, ...props}) => {
+
+  const router = useRouter();
+  //to insure only one element is provided:
+  const child = Children.only(children);
+  let className = child.props.className || '';
+
+  if(router.asPath === props.href && props.activeClassName){
+    className = `${className} ${props.activeClassName}`
+  }
+  delete props.activeClassName;
+
+
+  return <Link {...props}>{React.cloneElement(child, {className})}</Link>
+}
+
+export default ActiveLink
